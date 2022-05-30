537bc17d61bc9f853f9ea46ff3132197eef8c012
Thauler <thauler98@gmail.com>
2022-05-30 18:39:55 -0300

    Initial commit

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,15 +2,13 @@ import * as express from 'express';
 
 class App {
   public app: express.Express;
-  // ...
 
   constructor() {
-    // ...
+    this.app = express();
     this.config();
-    // ...
   }
 
-  private config():void {
+  private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
@@ -19,12 +17,11 @@ class App {
     };
 
     this.app.use(accessControl);
-    // ...
+    this.app.use(express.json());
   }
 
-  // ...
   public start(PORT: string | number):void {
-    // ...
+    this.app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
   }
 }
 
